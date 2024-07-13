10be2efc5ea4b44f4c1d0f20ec1734f99a9896b1
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-13 20:08:33 +0200

    comment packer config

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-output "ubuntu_acme_frontend" {
-  value = data.hcp_packer_version.ubuntu_acme_frontend
-}
-
-output "aws_ubuntu_acme_frontend_img" {
-  value = data.hcp_packer_artifact.aws_ubuntu_acme_frontend_img
-}
-
+#output "ubuntu_acme_frontend" {
+#  value = data.hcp_packer_version.ubuntu_acme_frontend
+#}
+#
+#output "aws_ubuntu_acme_frontend_img" {
+#  value = data.hcp_packer_artifact.aws_ubuntu_acme_frontend_img
+#}
+#
 output "aws_acme_frontend" {
   value = [
     module.ec2["frontend"].id,
