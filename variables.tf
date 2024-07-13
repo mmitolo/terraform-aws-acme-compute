712b88eb600736d75540e3b9a84a64ba9d7b633a
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-13 20:11:33 +0200

    set azs default value

--- variables.tf
+++ variables.tf
@@ -72,6 +72,7 @@ variable "subnet_tags" {
 variable "azs" {
   type        = list(string)
   description = "A list of availability zones in the region"
+  default     = []
 }
 
 variable "enable_nat_gateway" {
