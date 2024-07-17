d3e77c365a5304612c1c8df22c0a7ef2a03ebf0e
Mauricio Mitolo <mauricio.estebanmitolo@hashicorp.com>
2024-07-17 09:07:57 +0200

    update output data

--- output.tf
+++ output.tf
@@ -1,13 +1,15 @@
-#output "ubuntu_acme_frontend" {
-#  value = data.hcp_packer_version.ubuntu_acme_frontend
-#}
-#
-#output "aws_ubuntu_acme_frontend_img" {
-#  value = data.hcp_packer_artifact.aws_ubuntu_acme_frontend_img
-#}
-#
-output "aws_acme_frontend" {
-  value = [
-    module.ec2["frontend"].id,
-  module.ec2["frontend"].public_ip]
+output "ubuntu_acme_frontend" {
+  value = data.hcp_packer_version.ubuntu_acme_frontend
+}
+
+output "aws_ubuntu_acme_frontend_img" {
+  value = data.hcp_packer_artifact.aws_ubuntu_acme_frontend_img
+}
+
+output "aws_acme_frontend_public_ip" {
+  value = module.ec2["frontend"].public_ip
+}
+
+output "aws_acme_frontend_public_dns" {
+  value = module.ec2["frontend"].public_dns
 }
